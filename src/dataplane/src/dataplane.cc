6d5b5bae2d55953b4f39238a3d4cbe8e7247bbce
VenkatKunaparaju <venkatkrox@gmail.com>
2023-04-20 14:37:55 -0400

    Headers

--- src/dataplane/src/dataplane.cc
+++ src/dataplane/src/dataplane.cc
@@ -4,6 +4,7 @@
 /*
  Poll in startup state until simpleP4 program compiled 
 Return: 1 on success and population of respective fields
+*/
 int startUp() {
     pipe(p4ToDataplane);
     while(writtenToDataplane == 0); //Poll until writtenToDataplane is 1
@@ -12,7 +13,6 @@ int startUp() {
     writtenToDataplane = 0; //Reset value to 0 once done reading
     return 1;
 }
-*/
 int main() {
     std::cout << "DATAPLANE\n";
     return 0;
--- src/simplep4/compile.hh
+++ src/simplep4/compile.hh
@@ -1,21 +1,30 @@
 
-/* Example: How a header should get translated to, map the bits the user specifies into a char array 
+/* Example file */
+
+/* How a header should get translated to, map the bits the user specifies into a char array 
 of the rounded up to nearest bytes. Name is the same as the user specifies 
-For example "x bit<17>" -> 
+For example "x bit<17>" -> "char x[3]"
 */
 struct header {
-    char source_ip[32];
-    char dest_ip[32];
-    char source_port[32];
-    char dest_port[32];
-    char type[4];
+    char source_ip[4];
+    char dest_ip[4];
+    char source_port[4];
+    char dest_port[4];
+    char type[1];
 };
 
-struct metadata {
+/* Used to keep track of how many bits each entry actually takes up */
+struct headerMetadata {
     int source_ip;
     int dest_ip;
     int source_port;
-    int
+    int dest_port;
+    int type;
+};
 
+/* Used to keep track of metadata that user might specify */
+struct metadata {
+    int ingress_port;
+    int egresss_port; 
 };
 
