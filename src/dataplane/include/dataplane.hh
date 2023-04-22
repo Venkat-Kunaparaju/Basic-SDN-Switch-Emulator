1f0593c1a5981a08c80b9489ce88c4d87b9c555b
VenkatKunaparaju <venkatkrox@gmail.com>
2023-04-22 16:35:13 -0400

    Planning

--- src/dataplane/include/dataplane.hh
+++ src/dataplane/include/dataplane.hh
@@ -8,6 +8,8 @@ int writtenToDataplane; //Set to 0 if data is empty, 1 if data exists
 /* Vectors used to validate information */
 std::vector<std::string> header; //Collection of Fields of header of packet
 std::vector<int> headerMetadata; //Parallel to header vector to contain bits considered in field
+
+/* Vectors shouldn't change across compile requests */
 std::vector<std::string> metadata; //Collection of Metadata given to packet during pipeline2
 std::vector<std::string> actions; //Collection of Actions potentially to give to packet
 
@@ -15,13 +17,20 @@ struct fields
 {
     std::vector<std::string> fields; //Includes both metadata and packet header
     std::vector<char *> value;       //Parallel with fields vector and shows values of field to match with
-    int numFields;
+
+    int numFields; //Number of fields being considered in this entry
 };
 
 struct table
 {
+    std::vector<std::string> exactFields; //Fields that need to be spcified
+    std::vector<std::string> couldFields; //Fields that could be specified
+
     std::vector<fields> fieldsVect;   //Fields to match with in an entry; Collection of entries
     std::vector<std::string> actions; //Parallel with fieldsVect; Actions associated with entry.
+    std::string default_action; //Default action if no matching entries
+
+    int maxNumEntries;
     int numEntries;
 };
 
--- src/simplep4/compile.hh
+++ src/simplep4/compile.hh
@@ -28,3 +28,28 @@ struct metadata {
     int egresss_port; 
 };
 
+/* Specify matching fields in simple p4 */
+/*
+Fields can be either sepcified as exact, or could match. 
+Control plane needs to mention exact fields, and could mention could fields.
+*/
+
+/* Protocol for communication between simplep4 and dataplane */
+/*
+Variables
+X = Table<table num>:<exact field names seperated by commas (includes comma after last field name)><space>
+<exact field names seperated by commas (includes comma after last field name)><space>
+<could field names seperated by commas (includes comma after last field name)><space>
+<actions seperated by commas (includes comma after last action)><space><default action><space>
+<max number of entries><space>
+
+
+*ACTUAL PROTOCOL*
+SYN<space><header fields speerated by commas (includes comma after last field name)><space>
+<header metadata seperated by commas(includes comma after last metadata)><space>
+NumTables:<number of tables><space>
+<X[i] for i in tables>
+
+
+*/
+
