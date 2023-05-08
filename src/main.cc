02303431d1c616d1e479cdb1f1c89bedd4841437
VenkatKunaparaju <venkatkrox@gmail.com>
2023-05-08 18:19:16 -0400

    Comment

--- src/main.cc
+++ src/main.cc
@@ -1,5 +1,7 @@
 #include <main.hh>
 
+//Make multiple threads, each to handle the different component of the application
+//Makes dealing with shared resources muche easier
 int main() {
     fprintf(stderr, "CHECK\n");
     startUp();
